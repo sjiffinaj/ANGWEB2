fbc0ea5e98ec2d1f625ade59dce011df80e287e9
SJ <sjiffin20@gmail.com>
2022-10-29 21:27:43 +0530

    client changes

--- Client/WebClient/src/app/app.module.ts
+++ Client/WebClient/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SignupComponent } from './components/signup/signup.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/WebClient/src/app/components/login/login.component.ts
+++ Client/WebClient/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  type :string = "password"
+  istext :boolean = false
+  eyeIcon :string = "fa-eye-slash"
+  loginForm!:FormGroup; 
+
+
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username:['', Validators.required],
+      password:['', Validators.required]
+
+    })
   }
 
+  showPass(){
+    this.istext = !this.istext;
+    this.istext ? this.type="text" : this.type="password";
+    this.istext ? this.eyeIcon="fa-eye-slash" : this.eyeIcon="fa-eye";
+
+  }
+
+  OnSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      console.log("Login is success");
+
+    }else{
+      this.validateAllFormFields(this.loginForm);
+      alert("Invalid Form");
+    }
+  }
+
+  private validateAllFormFields(formGroup : FormGroup){
+    Object.keys(formGroup.controls).forEach(field =>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl)
+      {
+        control.markAsDirty({onlySelf:true});
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control);
+      }
+
+    });
+  }
 }
